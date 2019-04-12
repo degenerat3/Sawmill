0dc05925a133d198a969022ff7628ca9c98dd279
Micah J. Martin <mjm5097@rit.edu>
2019-04-11 20:56:29 -0400

    Updated scripts and made the types 'USERNAME' to support ymbols and char

--- scripts/clearElastic.sh
+++ scripts/clearElastic.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -X DELETE 'http://localhost:9200/_all'
+if [ "$SYSLOG_HOST" = "" ] && [ "$1" = "" ]; then
+	echo "Please pass the IP address as the first argument or in the SYSLOG_HOST env var."
+	exit
+fi
+[ "$1" != "" ] && HOST="$1" || HOST="$SYSLOG_HOST"
+curl -X DELETE "http://$HOST:9200/_all"
